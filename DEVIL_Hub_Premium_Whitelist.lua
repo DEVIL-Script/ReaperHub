d233a95dc6dc79c0c9ce27136a44ff58b485ade5
DEVIL-Script <118387949+DEVIL-Script@users.noreply.github.com>
2025-06-22 21:42:15 +0545

    Update whitelist

--- DEVIL_Hub_Premium_Whitelist.lua
+++ DEVIL_Hub_Premium_Whitelist.lua
@@ -1,4 +1,4 @@
 local DiscordWhitelist = {
-          ["GRwcEh8d"] = {discordId = "931823254195294228", active = true},
+          ["EhscEhITGx0Z"] = {discordId = "931823254195294228", active = true},
     }
     return DiscordWhitelist
