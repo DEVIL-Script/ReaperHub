49d978cbb3b791f9e4caccd0b9178eeba9424fa3
DEVIL-Script <118387949+DEVIL-Script@users.noreply.github.com>
2025-06-23 20:08:15 +0545

    Update DEVIL_Hub_Premium_Whitelist.lua

--- DEVIL_Hub_Premium_Whitelist.lua
+++ DEVIL_Hub_Premium_Whitelist.lua
@@ -1,10 +1,1 @@
-local DiscordWhitelist = {
-    ["GB4cHxoaGBwYHw=="] = {discordId = "931823254195294228", active = true},
-    ["HBwaHRoTHRMf"] = {discordId = "465610916873109504", active = true},
-    ["Eh0ZEhsZGB4dGA=="] = {discordId = "931823254195294228", active = false},
-    ["GBsTGRMcEw=="] = {discordId = "931823254195294228", active = true},
-    ["EhIdHB8Z"] = {discordId = "931823254195294228", active = true},
-    ["GBISEhoSHg=="] = {discordId = "931823254195294228", active = true},
-    ["GxwbGxsfGw=="] = {discordId = "931823254195294228", active = false},
-}
-return DiscordWhitelist
+
